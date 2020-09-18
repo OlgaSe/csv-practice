f1827e56a21701758dd42e8f00e8b59098869054
Olga Sedysheva <olga.tka4eva@gmail.com>
2020-09-18 00:13:32 -0700

    couple tests still  fail

--- lib/csv_practice.rb
+++ lib/csv_practice.rb
@@ -2,11 +2,24 @@ require 'csv'
 require 'awesome_print'
 
 def get_all_olympic_athletes(filename)
-
+  athlete_fields = %w[ID Name Height Team Year City Sport Event Medal]
+  olympic_athletes = []
+  CSV.read('../data/athlete_events.csv', headers: true).each do |athlete_row|
+    athlete = {}
+    athlete_fields.each do |field_name|
+      athlete[field_name] = athlete_row[olympic_athletes]
+    end  
+    olympic_athletes << athlete
+  end
+  return olympic_athletes
 end
 
-def total_medals_per_team(olympic_data)
 
+def total_medals_per_team(olympic_data)
+  athletes_with_medals = olympic_data.filter {|athlete| athlete[:Medal] != 'NA' }
+  athletes_grouped_by_team = athletes_with_medals.group_by {|athlete| athlete[:Team]}
+  all_teams_with_medals = athletes_grouped_by_team.map {|team, athletes| [team, athletes.length]}
+  return all_teams_with_medals.to_h
 end
 
 def get_all_gold_medalists(olympic_data)
